ce8ecd2e63320c0ce446b6c932930ce42c2f46fb
Bikash <khanalbikash55@gmail.com>
2022-10-31 16:11:22 +0545

    Dependency Injection for UnitOfWork

--- Hotel_Reservation_System/Program.cs
+++ Hotel_Reservation_System/Program.cs
@@ -1,4 +1,6 @@
 using Hotel_Reservation_System.Context;
+using Hotel_Reservation_System.Repositories;
+using Hotel_Reservation_System.Repositories.IRepositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
