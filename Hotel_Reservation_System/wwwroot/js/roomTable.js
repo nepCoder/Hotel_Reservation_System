286593c52878f4497401c8ab442035123289888f
Bikash <khanalbikash55@gmail.com>
2022-11-06 21:45:32 +0545

    js function to call api for datatables

---
+++ Hotel_Reservation_System/wwwroot/js/roomTable.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('#roomTable').DataTable({
+        "ajax":{
+            "url": "/Rooms/GetAll"
+        },
+        "columns":[
+            { "data": "roomName", "width": "15%" },
+            { "data": "roomDescription", "width": "15%" },
+            { "data": "roomCategoryId", "width": "15%" },
+            { "data": "cost", "width": "15%" },
+            { "data": "status", "width": "15%" }
+        ]
+    });
+});
